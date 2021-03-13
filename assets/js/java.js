e7eb94a5f282a14b9f34e0ad5384e388c67d0b99
Jesse Orozco <orozcojesse0@gmail.com>
2021-03-12 19:41:16 -0500

    trying to apply time

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,13 +1,51 @@
-let timerEl = document.getElementById("timer");
-let quizGenerator = document.getElementById("quiz");
+let timerEl = document.querySelector(".timer");
+let quizGenerator = document.getElementById("quizTitle");
 let viewResults = document.getElementById("result");
 let quizSubmit = document.getElementById("submit");
+let setTimer = document.getElementById("countdown");
 
-function generate() {}
-function results() {}
+const secondsLeft = 10;
 
-let quizTitle = "This is my quiz";
-document.getElementById("quizTitle").innerHTML = quizTitle;
+function setTime() {
+  const timerInterval = setInterval(function () {
+    secondsLeft--;
+    timerEl.textContent = secondsLeft + " countdown until the next question.";
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+
+      sendMessage();
+    }
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+  timerEl.textContent = " ";
+  var imgEl = document.createElement("img");
+  timerEl.setAttribute("src", "question2-question");
+  mainEl.appendChild(imgEl);
+}
+
+setTime();
+
+// let quizTitle = "This is my quiz";
+// document.getElementById("quizTitle").innerHTML = quizTitle;
+
+// const startingMinutes = 1;
+// let time = startingMinutes * 60;
+
+// const countdownEl = document.getElementById("countdown");
+
+// setInterval(updateCountdown, 1000);
+
+// function updateCountdown() {
+//   const minutes = Math.floor(time / 60);
+//   let seconds = time % 60;
+
+//   countdownEl.innerHTML = "${minutes}:${seconds}";
+//   time--;
+// }
 // function myFunction() {
 //   var x = document.createElement("start-button");
 //   x.setAttribute("type", "number");
@@ -57,31 +95,45 @@ document.getElementById("question1-B").innerHTML = questionOne.b;
 document.getElementById("question1-C").innerHTML = questionOne.c;
 document.getElementById("question1-D").innerHTML = questionOne.d;
 
-// let solveSolutions = [
-//   {
-//     question: "In what year was javascript invented?",
-//     a: "1990",
-//     b: "1995",
-//     c: "2000",
-//     d: "2021",
-//     answer: "b",
-//   },
+let questionTwo = {
+  question: "is javascript and java the same programming language?",
+  a: true,
+  b: false,
+  answer: "false",
+};
 
-//   {
-//     question: "is javascript and java the same programming language?",
-//     a: true,
-//     b: false,
-//     answer: "false",
-//   },
+function answerQuestionTwo(optionSelected, correctAnswer) {
+  if (optionSelected == correctAnswer) {
+    alert("Correct Answer");
+  } else {
+    alert("Wrong Answer");
+  }
+}
 
-//   {
-//     question:
-//       "Javascript can use the document.getElementById to acces an HTML element?",
-//     a: true,
-//     b: false,
-//     answer: "true",
-//   },
-// ];
+document.getElementById("question2-question").innerHTML = questionTwo.question;
+document.getElementById("question2-A").innerHTML = questionTwo.a;
+document.getElementById("question2-B").innerHTML = questionTwo.b;
+
+let questionThree = {
+  question:
+    "Javascript can use the document.getElementById to acces an HTML element?",
+  a: true,
+  b: false,
+  answer: "true",
+};
+
+function answerQuestionThree(optionSelected, correctAnswer) {
+  if (optionSelected == correctAnswer) {
+    alert("Correct Answer");
+  } else {
+    alert("Wrong Answer");
+  }
+}
+
+document.getElementById("question3-question").innerHTML =
+  questionThree.question;
+document.getElementById("question3-A").innerHTML = questionThree.a;
+document.getElementById("question3-B").innerHTML = questionThree.b;
 
 //gets the elements from the above questions.
 function get(x) {
